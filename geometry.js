46bd93222ed1cef883aa5f2c48a2b2739bfc463e
awarestudio <awarestudio.cc@gmail.com>
2023-04-04 12:22:02 +0200

    Changed Point2D and Line2D into Pattern2D

--- geometry.js
+++ geometry.js
@@ -1,12 +1,29 @@
-class Point2D {
-    constructor(x, y) {
-        this.x = x
-        this.y = y
+class Pattern2D {
+    constructor(points) {
+        this.points = points
+    }
+
+    static fromPoints(points) {
+        return new Pattern2D(points)
+    }
+
+    debug(size=1) {
+        for (let index = 0; index < this.points.length; index++) {
+            ellipse(this.points[index].x, this.points[index].y, size)
+        }
     }
 }
 
-class Line2D {
-    constructor(points) {
-        this.points = points
+class Line2D extends Pattern2D {
+    static fromTwoPoints(start, end, num) {
+        let xArray = linspace(start.x, end.x, num, true)
+        let yArray = linspace(start.y, end.y, num, true)
+
+        let points = []
+        for (let index = 0; index < num; index++) {
+            points[index] = createVector(xArray[index], yArray[index])
+        }
+
+        return this.fromPoints(points)
     }
 }
